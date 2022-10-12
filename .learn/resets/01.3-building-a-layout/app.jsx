a5f56ae79481bb9f89bd16041d6d46362fc4d4e6
lusferror <luisalfredorobles09@gmail.com>
2022-10-12 14:09:25 +0000

    primera fase de tutorial react

--- .learn/resets/01.3-building-a-layout/app.jsx
+++ .learn/resets/01.3-building-a-layout/app.jsx
@@ -13,20 +13,7 @@ const data = {
 };
 
 let content = (
-	/*(<img src={data.image} />*/
-	<div className="card m-5">
-		<img className="card-img-top" src={data.image} alt="Card image cap" />
-		<div className="card-body">
-			<h5 className="card-title">Bob Dylan</h5>
-			<p className="card-text">
-				Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential
-				figure in popular music and culture for more than five decades.
-			</p>
-			<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
-				Go to wikipedia
-			</a>
-		</div>
-	</div>
+		<img src={data.image} />
 );
 /**
  * define the variable 'content' here and fill it with the
--- exercises/01.4-building-html-from-arrays/app.jsx
+++ exercises/01.4-building-html-from-arrays/app.jsx
@@ -1,9 +1,20 @@
+import { element } from "prop-types";
 import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const navlinkItems = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+const listItems = navlinkItems.map((item) => {
+	return (
+		<li className="nav-item">
+			<a className="nav-link" href="#">
+				{item}
+			</a>
+		</li>
+	);
+});
+
+const content = <ul className="nav">{listItems}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
